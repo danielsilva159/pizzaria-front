eaa1e420998a9f927536e14fcf5077a978b1a692
Daniel Ferreira <silva_120000@yahoo.com.br>
2022-06-20 07:32:10 -0300

    Configurando api e fazendo acesso pela tela de login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,26 @@
-import type { NextPage } from 'next'
+import { FormEvent, useContext, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import logoImg from '../../public/logo.svg'
 import styles from '../../styles/home.module.scss'
 import {Input} from '../components/ui/input'
 import {Button} from '../components/ui/button';
+import {AuthContext} from '../contexts/AuthContext';
+import Link from 'next/link'
 
 export default function Home(){
+  const {signIn} = useContext(AuthContext)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleLogin(event: FormEvent){
+    event.preventDefault();
+    let data = {
+      email,
+      password    }
+    await signIn(data)
+  }
   return (
     <>
     <Head>
@@ -15,12 +29,14 @@ export default function Home(){
     <div className={styles.containerCenter}>
       <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
       <div className={styles.login}>
-        <form>
-          <Input placeholder='Digite seu email' type='text' />
-          <Input placeholder='Digite sua senha' type='password' />
+        <form onSubmit={handleLogin}>
+          <Input placeholder='Digite seu email' type='text' value={email} onChange={(e) => setEmail(e.target.value)} />
+          <Input placeholder='Digite sua senha' type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
           <Button type='submit' loading={false}>Acessar</Button>
         </form>
+        <Link href="/signup">
         <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+        </Link>
       </div>
     </div>
     </>
